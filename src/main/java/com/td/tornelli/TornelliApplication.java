32b3539a3a34402f4971cb0440eb9d8a8fa5c426
Jacopo Allori <jacopo.allori@eng.it>
2025-05-20 14:45:19 +0200

    Inizializzazione progetto Spring Boot

---
+++ src/main/java/com/td/tornelli/TornelliApplication.java
@@ -0,0 +1,13 @@
+package com.td.tornelli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TornelliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TornelliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/td/tornelli/TornelliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.td.tornelli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TornelliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
